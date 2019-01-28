eded28e2d91b522735b6c6d55c95f929b78b8e17
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-01-28 13:20:01 -0500

    Get the object bouncing

--- bouncing.fs
+++ bouncing.fs
@@ -4,11 +4,14 @@ include physics.fs
 
 create obj objSize cells allot
 
-( width height x y  -- [no return] )
+( width height x y vx vy  -- [no return] )
 : mainLoop
+	obj objInit
 	begin
 		clearScreen
-		80 40 14 28 drawScreen
+		2dup obj objMove
+		2dup obj objX @ obj objY @ drawScreen
 		100 ms
 	again
+	drop drop
 ;
